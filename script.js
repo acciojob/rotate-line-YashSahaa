bcd4e7f26b6513e181778ef3fb37286fd1d7d33d
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-09 17:26:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let line = document.getElementById("line")
+let count=0
+
+setInterval(()=>{
+	count+=5
+	line.style.transform = `translate(-50%, -50%) rotate(${count}deg)`
+},50)
